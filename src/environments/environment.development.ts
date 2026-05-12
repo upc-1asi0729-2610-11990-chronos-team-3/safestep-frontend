4411a279ce03ba0cec4b1d24d3c2a3eb0cce6c11
Jorge Ayala <jayala42000@gmail.com>
2026-05-12 11:48:47 -0500

    chore: SafeStep json-server deployment Initialization!!

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: false,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
-  identityAccessEndpointPath: '/identity-access',
-  medicalSimulationsEndpointPath: '/medical-simulations',
+  platformProviderApiBaseUrl:
+    'https://my-json-server.typicode.com/upc-1asi0729-2610-11990-chronos-team-3/safestep-json-server',
+  identityAccessEndpointPath: '/identityAccess',
+  medicalSimulationsEndpointPath: '/medicalSimulations',
   gamificationEndpointPath: '/gamification',
   ecommerceEndpointPath: '/ecommerce',
-  statisticsEndpointPath: '/statistics'
+  statisticsEndpointPath: '/statistics',
 };
